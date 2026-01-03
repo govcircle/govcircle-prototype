0048ff32b25bbe33dde282817bb452ddb35956bb
sadgh <sghorogi@gmail.com>
2026-01-03 17:22:28 +0330

    chore: use complete path for base

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({mode}) => {
             port: 3000,
             host: '0.0.0.0',
         },
-        base: "/govcircle-prototype",
+        base: "/govcircle-prototype/",
         plugins: [react()],
         resolve: {
             alias: {
